afe76f74aebf96fec4b2c6bc429b4dd21855e518
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2023-11-08 10:33:09 +0400

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,41 +12,36 @@
 
 #include "libft.h"
 
+//strjoin concatenates two strings s1 and s2
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*all;
 	size_t	i;
 	size_t	j;
 
-	all = (char *)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!all)
-		return (NULL);
 	i = 0;
 	j = 0;
+//It first allocates memory for a new string all that is large enough to
+//hold both s1 and s2, plus an extra character for the null terminator ('\0').
+	all = (char *)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
+//if the memory allocation fails, the function returns NULL
+	if (!all)
+		return (NULL);
+//It then copies each character from s1 into all.
 	while (s1[i])
 	{
 		all[i] = s1[i];
 		i++;
 	}
+//After that, it appends each character from s2 to all.
 	while (s2[j])
 	{
 		all[i] = s2[j];
 		j++;
 		i++;
 	}
+//Finally, it adds a null terminator at the end of all and returns the newly created string.
 	all[i] = '\0';
 	return (all);
 }
-
-/* int main(void)
-{
-    char    *strs[5];
-
-    strs[0] = "it";
-    strs[1] = "is";
-    strs[2] = "what";
-	strs[3] = "it";
-	strs[4] = "is";
-	char sep[] = " ";
-    printf("%s", ft_strjoin(5, strs, sep));
-} */
