da9061b1701e78cc27ceae732c8a2155fad9e924
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2023-11-21 19:37:03 +0400

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -10,9 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-static int	wordcounter(char *str, char c)
+static int	wordcount(char *str, char c)
 {
 	int	i;
 	int	word;
@@ -35,19 +33,19 @@ static int	wordcounter(char *str, char c)
 	return (word);
 }
 
-static void	freeall(char **s, int j)
+static void	freemem(char **str, int i)
 {
-	if (!s)
+	if (!str)
 		return ;
-	while (s[j] != NULL)
+	while (i >= 0)
 	{
-		free(s[j]);
-		j++;
+		free(str[i]);
+		i--;
 	}
-	free(s);
+	free (str);
 }
 
-static char	allocation(const char *s, char c)
+static char	**allocation(const char *s, char c)
 {
 	char	**all;
 	int		word;
@@ -56,11 +54,11 @@ static char	allocation(const char *s, char c)
 	{
 		all = malloc(sizeof(char *));
 		if (!all)
-			return ;
+			return (NULL);
 		all[0] = NULL;
-		return ((char *)all);
+		return (all);
 	}
-	word = wordcounter((char *)s, c);
+	word = wordcount((char *)s, c);
 	all = malloc(sizeof(char **) * (word + 1));
 	if (!all)
 		return (NULL);
@@ -70,52 +68,153 @@ static char	allocation(const char *s, char c)
 
 static void	substring(char **all, const char *s, char c, int word)
 {
-	size_t	i;
-	int		end;
+	int		j;
 	size_t	len;
+	size_t	i;
 
+	j = 0;
 	i = 0;
-	end = 0;
 	while (s[i])
 	{
 		len = 0;
-		while (s[i + len] != c && s[i])
+		while (s[i + len] != c && s[i + len])
 			len++;
-		if (len && end < word)
+		if (len && j < word)
 		{
-			all[end] = ft_substr(s, i, len);
-			if (!all[end])
+			all[j] = ft_substr(s, i, len);
+			if (!all[j])
 			{
-				freeall(all, end);
+				freemem(all, j);
 				return ;
 			}
-			end++;
+			j++;
 		}
 		i += len;
 		i++;
 	}
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	char	**mem;
+	char	**all;
 	int		word;
 
 	if (!s)
 		return (NULL);
-	mem = allocation(s, c);
-	if (!mem)
-		return ;
+	all = allocation(s, c);
+	if (!all)
+		return (NULL);
 	if (*s == '\0' && c != '\0')
 	{
-		mem[0] = NULL;
-		return (mem);
+		all[0] = NULL;
+		return (all);
 	}
-	word = wordcounter((char *)s, c);
-	substring(mem, s, c, word);
-	return (mem);
+	word = wordcount((char *)s, c);
+	substring(all, s, c, word);
+	return (all);
+}static int	wordcount(char *str, char c)
+{
+	int	i;
+	int	word;
+
+	i = 1;
+	word = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] != c)
+		{
+			word++;
+			while (str[i] != c && str[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
 }
 
+static void	freemem(char **str, int i)
+{
+	if (!str)
+		return ;
+	while (i >= 0)
+	{
+		free(str[i]);
+		i--;
+	}
+	free (str);
+}
+
+static char	**allocation(const char *s, char c)
+{
+	char	**all;
+	int		word;
+
+	if (s == NULL || *s == '\0')
+	{
+		all = malloc(sizeof(char *));
+		if (!all)
+			return (NULL);
+		all[0] = NULL;
+		return (all);
+	}
+	word = wordcount((char *)s, c);
+	all = malloc(sizeof(char **) * (word + 1));
+	if (!all)
+		return (NULL);
+	all[word] = 0;
+	return (all);
+}
+
+static void	substring(char **all, const char *s, char c, int word)
+{
+	int		j;
+	size_t	len;
+	size_t	i;
+
+	j = 0;
+	i = 0;
+	while (s[i])
+	{
+		len = 0;
+		while (s[i + len] != c && s[i + len])
+			len++;
+		if (len && j < word)
+		{
+			all[j] = ft_substr(s, i, len);
+			if (!all[j])
+			{
+				freemem(all, j);
+				return ;
+			}
+			j++;
+		}
+		i += len;
+		i++;
+	}
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**all;
+	int		word;
+
+	if (!s)
+		return (NULL);
+	all = allocation(s, c);
+	if (!all)
+		return (NULL);
+	if (*s == '\0' && c != '\0')
+	{
+		all[0] = NULL;
+		return (all);
+	}
+	word = wordcount((char *)s, c);
+	substring(all, s, c, word);
+	return (all);
+}
 // int main() 
 // {
 //     char  input_string[] = "";
