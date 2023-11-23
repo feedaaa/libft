47caed867172da15252d05040d41dd406b09feab
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2023-11-23 18:34:59 +0400

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -31,6 +31,10 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		if (result >= LONG_MAX / 10 && sign == 1)
+			return (-1);
+		else if (result >= LONG_MAX / 10 && sign == -1)
+			return (0);
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
