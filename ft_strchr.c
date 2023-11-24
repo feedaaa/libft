32616de91b7b3b25be0f6b4a597e8b5e305f1bd0
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2023-11-24 14:34:07 +0400

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,16 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		if (s[i] == (char )c)
+			return ((char *)s + i);
+		i++;
 	}
-	if (!c && s[i] == '\0')
+	if (s[i] == (char )c)
 		return ((char *) s + i);
-	return (0);
+	return (NULL);
 }
